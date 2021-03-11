2dbd37503ab449981acbfb86f98d54580b4d6a92
Crowdbotics <team@crowdbotics.com>
2021-03-11 16:01:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sharedstory_24977/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sharedstory_24977;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "sharedstory_24977";
+    }
+}
